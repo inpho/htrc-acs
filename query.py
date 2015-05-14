64d9f837d987c3b900946a894fb40cffa4690634
Jaimie Murdock <jaimie.murdock@gmail.com>
2015-05-14 09:24:35 -0400

    auto training of each LoC class

--- query.py
+++ query.py
@@ -1,11 +1,14 @@
 from collections import namedtuple
 import httplib
+import os
 import os.path
+import subprocess
 import time
 
 from topicexplorer.lib import hathitrust
 import rdflib
 import skos
+
 
 LCCO_URI = 'http://inkdroid.org/lcco/'
 LCC = namedtuple('LCC', ['cls', 'subcls', 'topic'])
@@ -23,6 +26,10 @@ PORT = 6079
 BASE_URL = '/getid?category={0}'#'&number={1}'
 COUNT_URL = '/findidcount?category={0}'
 
+CONFIG_PATH = '/var/htrc-loc/config/'
+if not os.path.exists(CONFIG_PATH):
+    os.makedirs(CONFIG_PATH)
+
 for item in loader.keys()[:10]:
     time.sleep(1)
     cls = item.rsplit('/', 1)[-1]
@@ -35,10 +42,19 @@ for item in loader.keys()[:10]:
             htids = [i for i in data.split('|') if i]
             if htids:
                 print "FOUND", cls, len(htids)
-                output_path = os.path.join('/var/htrc-loc/', cls)
+                output_path = os.path.join('/var/htrc-loc/samples/', cls)
                 if len(htids) > 10:
                     htids = htids[:10]
                 hathitrust.download_vols(htids, output_path)
+
+                os.chdir('/var/htrc-loc/config/')
+                subprocess.call(["vsm", "init", "--htrc", output_path])
+                #subprocess.call(["vsm", "prep", cls + ".ini", "--lang", "en"])
+                subprocess.call(["vsm", "train", cls + ".ini",
+                                  "--context-type", "book",
+                                  "-p", "8",
+                                  "--iter", "500",
+                                  "-k", "20", "40", "60", "80"])
             else:
                 print "NONE ", cls
         else:
