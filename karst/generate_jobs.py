6b79e2496bd6b33a7c568b908afba16c81f5a506
Jaimie Murdock <jaimie.murdock@gmail.com>
2018-05-09 12:16:04 -0400

    better job processing

--- karst/generate_jobs.py
+++ karst/generate_jobs.py
@@ -1,8 +1,10 @@
 from glob import glob
 import subprocess
 
-for _, inifile in zip(range(10), glob('data/*.htids.txt.ini')):
-    area = inifile.replace('data/', '').replace('.htids.txt.ini', '')
-    print(f'SUBMITTING {area}')
-    subprocess.call(f'bash submit_qsub.sh {area}', shell=True)
+for i, inifile in zip(range(10), glob('../../data/*.htids.txt.ini')):
+    if i > 0:
+        area = inifile.replace('../../data/', '').replace('.htids.txt.ini', '')
+        print(f'SUBMITTING {area}')
+        for j in range(10):
+            subprocess.call(f'bash submit_qsub.sh {area}', shell=True)
 
--- karst/run_topicexplorer.sh
+++ karst/run_topicexplorer.sh
@@ -3,6 +3,16 @@ PATH=$HOME/anaconda3/bin/:$PATH
 export LANG=en_US.UTF-8 LANGUAGE=en_US.en LC_ALL=en_US.UTF-8
 which topicexplorer
 
-echo "PROCESSING: $INPHO_AREA"
-/usr/bin/time -v topicexplorer train -k 25 50 100 250 500 --iter 500 -p 16 --rebuild -q $INPHO_AREA
+AREA=${INPHO_AREA##*/}
+echo "$AREA"
+AREA=${AREA%.htids.txt.ini}
+DIRNAME=${INPHO_AREA%/*}
+SEED=`od -A n -t d -N 4 /dev/urandom | tr -d ' ' | tr -d '-'`
+NEW_CONFIG=$DIRNAME/datasets/$AREA/$AREA.$SEED.ini
+
+echo "PROCESSING: $AREA (seed: $SEED)"
+mkdir -p $DIRNAME/datasets/$AREA
+cp $INPHO_AREA $NEW_CONFIG
+
+/usr/bin/time -v topicexplorer train -k 25 50 100 250 500 --iter 500 -p 16 --rebuild --seed $SEED -q $NEW_CONFIG
 
