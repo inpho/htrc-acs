a470e90bdc24b42c24fe2a5e4a2f0c3bb5897c58
Jaimie Murdock <jaimie.murdock@gmail.com>
2018-05-12 21:12:52 -0400

    adding .span and .acsexp job tags

--- karst/submit_acsexp.sh
+++ karst/submit_acsexp.sh
@@ -7,5 +7,5 @@ export LANG=en_US.UTF-8 LANGUAGE=en_US.en LC_ALL=en_US.UTF-8
 AREA=$1
 
 module load gnu-parallel
-parallel qsub -q preempt -N htrc-loc.$AREA -o $ACS_PATH/logs/$AREA.acsexp.out -e $ACS_PATH/logs/$AREA.acsexp.err -m ae -M jammurdo@indiana.edu -l walltime=12:00:00 -l nodes=1:ppn=8 -v INPHO_AREA=$ACS_PATH/$AREA.htids.txt.ini,K={} run_acsexp.sh ::: 25 50 100 250 500
+parallel qsub -q preempt -N htrc-loc.$AREA.acsexp -o $ACS_PATH/logs/$AREA.acsexp.out -e $ACS_PATH/logs/$AREA.acsexp.err -m ae -M jammurdo@indiana.edu -l walltime=12:00:00 -l nodes=1:ppn=8 -v INPHO_AREA=$ACS_PATH/$AREA.htids.txt.ini,K={} run_acsexp.sh ::: 25 50 100 250 500
 
