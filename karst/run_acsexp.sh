a91c26d5eadc1ee560049679cf58972396ada334
Jaimie Murdock <jaimie.murdock@gmail.com>
2018-05-12 21:24:58 -0400

    fixing iterations and samples balance

--- karst/run_acsexp.sh
+++ karst/run_acsexp.sh
@@ -6,4 +6,4 @@ export LANG=en_US.UTF-8 LANGUAGE=en_US.en LC_ALL=en_US.UTF-8
 which topicexplorer
 
 cd $HOME/acs/htrc-acs/
-python acsexp.py --log-dir $ACS_PATH/results --samples 10 --iter 500 -k $K $INPHO_AREA
+python acsexp.py --log-dir $ACS_PATH/results --samples 50 --iter 250 -k $K $INPHO_AREA
